f71f7afdc9e12f1143c669569cbe3ec3616c6280
Claudia Matosa <claudiamatosa@users.noreply.github.com>
2018-06-10 13:47:43 +0100

    Test structure

---
+++ cypress/support/commands.js
@@ -0,0 +1,67 @@
+import { startsWith, toPairs } from 'ramda';
+
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add("login", (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This is will overwrite an existing command --
+// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+const fixturePrefix = 'fixture:';
+const graphQLPath = 'http://localhost:4000/graphql';
+
+// Allows stubbing GraphQL responses by operation name.
+Cypress.Commands.add('visitStubbed', function(url, operations = {}) {
+  const operationData = {};
+
+  toPairs(operations).forEach(([operation, stub]) => {
+    if (startsWith(fixturePrefix, stub)) {
+      // The stub starts with 'fixture' - load it from the fixtures folder.
+      cy.fixture(stub.replace(fixturePrefix, '')).then(data => {
+        operationData[operation] = data;
+      });
+    } else {
+      // The stub is free-form, return whatever's passed in
+      operationData[operation] = stub;
+    }
+  });
+
+  cy.visit(url, {
+    onBeforeLoad: win => {
+      cy.stub(win, 'fetch')
+        .withArgs(graphQLPath)
+        .callsFake(serverStub(operationData));
+    }
+  });
+});
+
+const serverStub = operations => (_, req) => {
+    const { operationName } = JSON.parse(req.body);
+    const resultStub = operations[operationName];
+    return resultStub ? Promise.resolve(responseStub(resultStub)) : {};
+  };
+  
+const responseStub = result => ({
+    json: () => Promise.resolve(result),
+    text: () => Promise.resolve(JSON.stringify(result)),
+    ok: true
+});
